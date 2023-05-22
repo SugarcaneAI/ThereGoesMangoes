c72d87afabfcc0a99dee98f22144b6d60a711660
SilverousBlack <juliancalebsegundo@gmail.com>
2023-05-22 09:28:21 +0800

    22052023

--- there_goes_mangoes/main.py
+++
@@ -1,7 +0,0 @@
-import RPi.GPIO as gpio
-import timeit
-
-def wait_for_echo(pin: int):
-    while not gpio.input(pin):
-        continue
-    return
---
+++ there_goes_mangoes/main.usonic.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as gpio
+import timeit
+from time import sleep
+
+gpio.setmode(gpio.BCM)
+
+gpio.setup()
+
+def wait_for_echo(pin: int):
+    while not gpio.input(pin):
+        continue
+    return
+
+def main():
+    while True:
+        gpio.output(26, 1)
+        sleep(0.00001)
+        gpio.output(26, 0)
+        
+
+if __name__ == "__main__":
+    main()
