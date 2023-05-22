de3ef8812cafc8b01554189cc82b4e00d97a6cdf
SilverousBlack <juliancalebsegundo@gmail.com>
2023-05-22 11:53:06 +0800

    Update main.usonic.py

--- there_goes_mangoes/main.usonic.py
+++ there_goes_mangoes/main.usonic.py
@@ -6,16 +6,11 @@ factory = gpio.pins.pigpio.PiGPIOFactory()
 R1 = gpio.DigitalOutputDevice(21, initial_value=1)
 R2 = gpio.DigitalOutputDevice(20, initial_value=1)
 
-R2.off()
-R1.off()
 try:
     SENSOR = gpio.DistanceSensor(echo=18, trigger=17, pin_factory=factory)
 except:
     sleep(1)
     SENSOR = gpio.DistanceSensor(echo=18, trigger=17, pin_factory=factory)
-
-R2.on()
-R1.on()
 
 def main():
     while True:
@@ -25,6 +20,9 @@ def main():
             sleep(0.25)
             R2.on()
             sleep(0.25)
+            R2.off()
+            R1.off()
+        else:
             R2.off()
             R1.off()
         sleep(0.5)
