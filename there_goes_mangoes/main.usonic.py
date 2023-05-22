211e8762e1a6edbc2ea4590a936933b731d562b8
SilverousBlack <juliancalebsegundo@gmail.com>
2023-05-22 13:18:19 +0800

    Update main.usonic.py

--- there_goes_mangoes/main.usonic.py
+++ there_goes_mangoes/main.usonic.py
@@ -13,25 +13,26 @@ except:
     SENSOR = gpio.DistanceSensor(echo=18, trigger=17, pin_factory=factory, partial=True)
 
 def main():
-    while not SENSOR.wait_for_out_of_range():
-        dist = 0
-        try:
-            dist = SENSOR.distance * 100
-        except: 
-            dist = dist
-            continue
-        print(f"Distance: {dist:.2f} cm", end="\r")
-        if 33 >= (dist) <= 35:
-            R1.on()
-            sleep(0.25)
-            R2.on()
-            sleep(0.25)
-            R2.off()
-            R1.off()
-        else:
-            R2.off()
-            R1.off()
-        sleep(0.5)
+    while True:
+        while not SENSOR.wait_for_out_of_range():
+            dist = 0
+            try:
+                dist = SENSOR.distance * 100
+            except: 
+                dist = dist
+                continue
+            print(f"Distance: {dist:.2f} cm", end="\r")
+            if 33 >= (dist) <= 35:
+                R1.on()
+                sleep(0.25)
+                R2.on()
+                sleep(0.25)
+                R2.off()
+                R1.off()
+            else:
+                R2.off()
+                R1.off()
+            sleep(0.5)
     print("script ended.")
 
 if __name__ == "__main__":
