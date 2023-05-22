7e0586964a37e72060ed445a149dec391de570b6
SilverousBlack <juliancalebsegundo@gmail.com>
2023-05-22 09:51:20 +0800

    Update main.usonic.py

--- there_goes_mangoes/main.usonic.py
+++ there_goes_mangoes/main.usonic.py
@@ -1,22 +1,12 @@
 import gpiozero as gpio
-import timeit
 from time import sleep
 
-gpio.setmode(gpio.BCM)
-
-gpio.setup()
-
-def wait_for_echo(pin: int):
-    while not gpio.input(pin):
-        continue
-    return
+SENSOR = gpio.DistanceSensor(24, 26)
 
 def main():
     while True:
-        gpio.output(26, 1)
-        sleep(0.00001)
-        gpio.output(26, 0)
-        
+        print(f"Distance: {SENSOR.distance * 100:.2f} cm", end="\r")
+        sleep(0.5)
 
 if __name__ == "__main__":
     main()
