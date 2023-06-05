19e3fc7b80fa0b89be44595b261e34eabe24b27c
SilverousBlack <juliancalebsegundo@gmail.com>
2023-06-06 02:17:15 +0800

    GIT AUTO SYNC

---
+++ there_goes_mangoes/util_proxi_ir.py
@@ -0,0 +1,36 @@
+from time import sleep
+
+import cv2
+import gpiozero as gpio
+
+from there_goes_mangoes.util.draw_crosshair import crosshair_norm
+
+cam = cv2.VideoCapture(0)
+
+while not cam.isOpened():
+    cam = cv2.VideoCapture(0)
+    sleep(0.01)
+    
+SENSOR = gpio.Button(21)
+
+color = (0, 0, 255)
+while True:
+    
+    _, image = cam.read()
+    
+    if SENSOR.is_pressed():
+        color = (0, 255, 0)
+    
+    cv2.namedWindow("Camera View: Ultrasonic", cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty("Camera View: Ultrasonic", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_AUTOSIZE)
+
+    image = crosshair_norm(image, 0.1, 0.1, 0.05, color=color)
+    
+    k = cv2.waitKey(1)
+    if k != -1:
+        break
+    else:
+        sleep(0.5)
+        
+cv2.destroyAllWindows()
+    
