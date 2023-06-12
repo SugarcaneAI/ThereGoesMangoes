74284c6813004e37cf16c2569dc5caedaa68cac1
SilverousBlack <juliancalebsegundo@gmail.com>
2023-06-12 09:44:30 +0800

    GIT AUTO SYNC

--- there_goes_mangoes/util_proxi_ir_tester.py
+++ there_goes_mangoes/util_proxi_ir_tester.py
@@ -7,7 +7,7 @@ factory = gpio.pins.pigpio.PiGPIOFactory()
 SENSOR = gpio.Button(21, pin_factory=factory)
 
 while True:
-    print(f"IR: {SENSOR.is_pressed}")
+    print(f"IR: {SENSOR.value}")
     sleep(0.125)
 
 
