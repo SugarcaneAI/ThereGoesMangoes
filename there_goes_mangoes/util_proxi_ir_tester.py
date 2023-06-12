794d0c802a7fb8e406f90e0f561722b21dcb4df0
SilverousBlack <juliancalebsegundo@gmail.com>
2023-06-12 09:51:26 +0800

    Create util_proxi_ir_tester.py

---
+++ there_goes_mangoes/util_proxi_ir_tester.py
@@ -0,0 +1,13 @@
+import gpiozero as gpio
+
+from time import sleep
+
+factory = gpio.pins.pigpio.PiGPIOFactory()
+    
+SENSOR = gpio.Button(21, pin_factory=factory)
+
+while True:
+    print(f"IR: {SENSOR.is_pressed}")
+    sleep(0.125)
+
+
