3974994ea18cb2cda1b5921ce10c23b1e31810c4
SilverousBlack <juliancalebsegundo@gmail.com>
2023-06-22 01:50:46 +0800

    22062023

--- there_goes_mangoes/main.py
+++ there_goes_mangoes/main.py
@@ -1,28 +1,51 @@
+
+import pathlib as pl
 from time import sleep
 
 import cv2
-
-import gpiozero as gpio
+import VL53L0X
 
 from ultralytics import YOLO
 
 from there_goes_mangoes.util.draw_crosshair import crosshair_norm
 
-factory = gpio.pins.pigpio.PiGPIOFactory()
+PARAM_MODEL = pl.Path(r"E:\Git\ThereGoesMangoes\model\nano-640\weights\best.pt")
+
+cam = cv2.VideoCapture(0)
+
+while not cam.isOpened():
+    cam = cv2.VideoCapture(0)
+    sleep(0.01)
+
+tof = VL53L0X.VL53L0X(i2c_bus=1,i2c_address=0x29)
+tof.open()
+tof.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+
+timing = tof.get_timing()
+if timing < 20000:
+    timing = 20000
+
+cv2.namedWindow("Camera View: Ultrasonic", cv2.WND_PROP_FULLSCREEN)
+cv2.setWindowProperty("Camera View: Ultrasonic", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_AUTOSIZE)
+
+results = YOLO()
 
 while True:
-    try:
-        SENSOR = gpio.DistanceSensor(echo=21, trigger=20, pin_factory=factory, partial=True)
+    ret, image = cam.read()
+    
+    dist = tof.get_distance() / 10
+    dist -= 4
+    
+    image = crosshair_norm(image, 0.1, 0.1, 0.05)
+    image = cv2.putText(image, f"{dist:.2f}cm", (5, 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), thickness=2)
+    
+    cv2.imshow("Camera View: Ultrasonic", image)
+    
+    k = cv2.waitKey(1)
+    if k != -1:
         break
-    except:
-        sleep(0.5)
+    else:
+        sleep(timing / 1000000)
     
-
-while True:
-    try:
-        dist = SENSOR.distance * 100
-    except:
-        sleep(0.25)
-        continue
-    
-    sleep(0.25)
+cam.release()
+cv2.destroyAllWindows()
