d9f411accad893b9d5d0c060297a050472d42d34
SilverousBlack <juliancalebsegundo@gmail.com>
2023-06-25 23:36:03 +0800

    AUTO COMMIT SERVICE - DEVELOPMENT ONLY

--- there_goes_mangoes/util_yolo_test.py
+++ there_goes_mangoes/util_yolo_test.py
@@ -9,12 +9,6 @@ WND_NAME = "Camera View"
 MODEL_PATH = pl.Path(r"model/torch.pt")
 
 model = YOLO(MODEL_PATH, task="detect")
-
-cam = cv2.VideoCapture(0)
-
-while not cam.isOpened():
-    cam = cv2.VideoCapture(0)
-    sleep(0.01)
     
 cv2.namedWindow(WND_NAME, cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty(WND_NAME, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_AUTOSIZE)
