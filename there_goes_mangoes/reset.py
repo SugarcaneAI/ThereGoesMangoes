eaab4405b46aac36ac730a496f287f72964b5072
SilverousBlack <juliancalebsegundo@gmail.com>
2023-07-01 08:17:38 +0800

    AUTO COMMIT SERVICE - DEVELOPMENT ONLY

--- there_goes_mangoes/reset.py
+++ there_goes_mangoes/reset.py
@@ -5,8 +5,8 @@ from time import sleep
 button = gpio.Button(6, pull_up=False)
 button.hold_time = 1
 
-button.when_held = lambda: print("sudo poweroff")
-button.when_released = lambda: print("sudo shutdown -r now")
+button.when_held = lambda: os.system("sudo poweroff")
+button.when_released = lambda: os.system("sudo shutdown -r now")
 
 while True:
     sleep(0.01)
