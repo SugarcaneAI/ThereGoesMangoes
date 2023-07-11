32c808192b1cbba3a881516cfbf90d69e1e1cc73
SilverousBlack <juliancalebsegundo@gmail.com>
2023-07-11 23:26:40 +0800

    AUTO COMMIT SERVICE - DEVELOPMENT ONLY

--- there_goes_mangoes/reset.py
+++ there_goes_mangoes/reset.py
@@ -8,7 +8,8 @@ VALVE = gpio.OutputDevice(21, active_high=False)
 button = gpio.Button(6, pull_up=False)
 button.hold_time = 1
 
-button.when_pressed = lambda: MOTOR.toggle(); VALVE.toggle()
+button.when_pressed = lambda: MOTOR.on(); VALVE.on()
+button.when_released = lambda: MOTOR.off(); VALVE.off()
 
 while True:
     sleep(0.01)
