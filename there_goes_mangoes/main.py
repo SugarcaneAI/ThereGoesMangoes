b4e23dd5627f5f17f7cb4b43c283183bb2994f05
SilverousBlack <juliancalebsegundo@gmail.com>
2023-08-10 23:37:23 +0800

    AUTO COMMIT SERVICE - DEVELOPMENT ONLY

--- there_goes_mangoes/main.py
+++ there_goes_mangoes/main.py
@@ -3,6 +3,7 @@ from time import sleep, time_ns
 
 import cv2
 import numpy as np
+import VL53L0X
 
 from ultralytics import YOLO
 
@@ -14,8 +15,10 @@ WND_NAME = "Camera View"
 PARAM_MODEL = pl.Path(__file__).parents[1].joinpath("model/torch.pt")
 # PARAM_MODEL = pl.Path(r"model/_train/small-256/weights/best.pt")
 
+XSHUT = gpio.OutputDevice(4)
 MOTOR = gpio.OutputDevice(20, active_high=False)
 VALVE = gpio.OutputDevice(21, active_high=False)
+XSHUT.off()
 
 MOTOR.off()
 VALVE.off()
@@ -25,43 +28,46 @@ cv2.setWindowProperty(WND_NAME, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_AUTOSIZE)
 
 model = YOLO(PARAM_MODEL)
 
-factory = gpio.pins.pigpio.PiGPIOFactory()
+tof = VL53L0X.VL53L0X(i2c_bus=1,i2c_address=0x29)
 
-SENSOR = gpio.Button(7, pull_up=True, pin_factory=factory)
+XSHUT.on()
 
+tof.open()
+
+tof.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+
+timing = tof.get_timing()
+if timing < 20000:
+    timing = 20000
+    
 cam = cv2.VideoCapture(0)
-
+    
 LOCK = False
 while True:
-
-    mintime = 0
-    maxtime = 0
-
-    if not LOCK:
+    
+    if not LOCK: 
         if not cam.isOpened():
             cam = cv2.VideoCapture(0)
             sleep(0.01)
         else:
             LOCK = True
-
+        
     while True:
-        otime = time_ns()
-
         _, image = cam.read()
         cap = time_ns()
         
         results = model.predict(
-            cv2.resize(image, dsize=(256, 192), interpolation=cv2.INTER_AREA),
-            stream=False,
-            conf=0.6,
+            cv2.resize(image, dsize=(256, 192), interpolation=cv2.INTER_AREA), 
+            stream=False, 
+            conf=0.6, 
             imgsz=(256, 192)
         )
         
         delay = time_ns() - cap
         
-        dist = SENSOR.is_pressed
+        dist = (tof.get_distance() / 10) - 2
             
-        image = cv2.putText(image, f"{dist}", (5, 270), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 255), thickness=2)
+        image = cv2.putText(image, f"{dist:.2f}cm", (5, 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), thickness=2)
         image = cv2.putText(image, f"{(delay / 1000000000):.2f}s @ {(1 / (delay / 1000000000)):.2f} FPS", (5, image.shape[0] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), thickness=2)
             
         nh, nw, _ = image.shape
@@ -109,7 +115,8 @@ while True:
                 if brdist <= 0.15:
                     color = (0, 255, 0)
                     
-                    TARGET = dist
+                    if 30 <= dist <= 35:
+                        TARGET = True                
                     
                 image = cv2.rectangle(image, (fbx, fby), (fex, fey), color=color, thickness=5)
                 
@@ -117,28 +124,26 @@ while True:
             image = crosshair_norm(image, 0.1, 0.1, 0.05, color=(0, 255, 0))
         else:
             image = crosshair_norm(image, 0.1, 0.1, 0.05)
-        
-        imintime = time_ns()
-        mintime = (imintime - otime) / 1000000000
-        image = cv2.putText(image, f"min: {mintime:.6f}s | max: {maxtime:.6f}s", (5, 235), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 255), thickness=2)
-        
+            
         cv2.imshow(WND_NAME, image)
         
         if TARGET:
+            tof.stop_ranging()
+            XSHUT.off()
             
+            tspray = ((dist - 30) / 5)
             VALVE.on()
+            sleep(1)
             
             _, image = cam.read()
             
             image = cv2.putText(image, f"PROCESSING", (5, 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), thickness=2)
             image = cv2.rectangle(image, (fbx, fby), (fex, fey), color=color, thickness=5)
-            image = cv2.putText(image, f"min: {mintime:.6f}s | max: {maxtime:.6f}s", (5, 235), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 255), thickness=2)
-            image = cv2.putText(image, f"{dist}", (5, 270), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 255), thickness=2)
             image = crosshair_norm(image, 0.1, 0.1, 0.05, color=(0, 255, 0))
             MOTOR.on()
             
             cv2.imshow(WND_NAME, image)
-            sleep(1)
+            sleep(1.25)
         
             #for ii in range(int(tspray * 10)):
             #    _, image = cam.read()
@@ -148,22 +153,16 @@ while True:
             #    
             #    cv2.imshow(WND_NAME, image)
             #    sleep(0.01)
-            sleep(0.5)
+            sleep(1 * tspray)
 
             MOTOR.off()
             VALVE.off()
-
-            imaxtime = time_ns()
-            maxtime = (imaxtime - otime) / 1000000000
-
-
             for ii in range(25):
                 _, image = cam.read()
                 
                 image = crosshair_norm(image, 0.1, 0.1, 0.05, color=(0, 255, 0))
                 image = cv2.putText(image, f"DOWNTIME: {(25 - ii) / 10}s", (5, 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), thickness=2)
-                image = cv2.putText(image, f"min: {mintime:.6f}s | max: {maxtime:.6f}s", (5, 235), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 255), thickness=2)
-                image = cv2.putText(image, f"{dist}", (5, 270), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 255), thickness=2)
+                
                 cv2.imshow(WND_NAME, image)
                 
                 k = cv2.waitKey(1)
@@ -171,21 +170,24 @@ while True:
                     break
                 else:
                     sleep(0.03) # sleep for 1 frame time
+            
+            tof = VL53L0X.VL53L0X(i2c_bus=1,i2c_address=0x29)
 
-        mintime = 0
-        maxtime = 0
-        
-        dist = False
-        
+            XSHUT.on()
+
+            tof.open()
+
+            tof.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+            
         k = cv2.waitKey(1)
         if k != -1:
             break
         else:
-            sleep(0.05)
+            sleep(timing / 1000000)
         
     k = cv2.waitKey(1)
     if k != -1:
         break
     else:
-        sleep(0.05)
+        sleep(timing / 1000000)
 
