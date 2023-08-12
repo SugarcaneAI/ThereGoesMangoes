265417aa70f8acff3b6ad0d3255b68507d3d8f80
SilverousBlack <juliancalebsegundo@gmail.com>
2023-08-12 18:27:28 +0800

    AUTO COMMIT SERVICE - DEVELOPMENT ONLY

--- there_goes_mangoes/reset.py
+++ there_goes_mangoes/reset.py
@@ -4,8 +4,8 @@ from time import sleep
 
 factory = gpio.pins.pigpio.PiGPIOFactory()
 
-MOTOR = gpio.OutputDevice(20, active_high=False, factory=factory)
-VALVE = gpio.OutputDevice (21, active_high=False, factory=factory)
+MOTOR = gpio.OutputDevice(20, active_high=False, pin_factory=factory)
+VALVE = gpio.OutputDevice (21, active_high=False, pin_factory=factory)
 
 button = gpio.Button(6, pull_up=False)
 button.hold_time = 1
