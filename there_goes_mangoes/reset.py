6984b48a1b30cdd12403a04b0777b3ab508e6e52
SilverousBlack <juliancalebsegundo@gmail.com>
2023-08-12 15:04:07 +0800

    AUTO COMMIT SERVICE - DEVELOPMENT ONLY

--- there_goes_mangoes/reset.py
+++ there_goes_mangoes/reset.py
@@ -3,13 +3,14 @@ import os
 from time import sleep
 
 MOTOR = gpio.OutputDevice(20, active_high=False)
-VALVE = gpio.OutputDevice(21, active_high=False)
+VALVE = gpio.OutputDevice (21, active_high=False)
 
 button = gpio.Button(6, pull_up=False)
 button.hold_time = 1
 
-button.when_pressed = lambda: MOTOR.on(); VALVE.on()
-button.when_released = lambda: MOTOR.off(); VALVE.off()
+#button.when_pressed = lambda: MOTOR.on(); VALVE.on()
+#button.when_released = lambda: MOTOR.off(); VALVE.off()
 
 while True:
+    print(button.value)
     sleep(0.01)
